ddeb59c20887dca2f8fd62009d61160aa4c259ba
archixs <arturshugo4@gmail.com>
2024-09-18 16:25:36 +0300

    Salabots to do list

--- to-do-list.php
+++ to-do-list.php
@@ -1,51 +1,62 @@
 <?php
 
-$stackList = ["first task", "second task"];
+// echo "This is my first CLI script in php\n";
+
+// for ($i = 1; $i <= 10; $i++) {
+//     echo "Number: $i\n";
+// }
+// echo "\n";
+// for ($i = 10; $i > 0; $i--) { 
+//     echo "Number: $i\n";
+// }
+
+// $persons = [
+//     ['name' => "Jānis", 'age' => 19],
+//     ['name' => "Dāvis", 'age' => 18],
+//     ['name' => "Anna", 'age' => 18]
+// ];
+
+// $input = readline("Ievadi savu vardu: ");
+// echo "Tavs vards ir $input\n";
+
+$taskList = ["first task", "second task"];
 
 function showAllTasks($inputTasks) {
     foreach($inputTasks as $task) {
-      echo $task . "\n"
+      echo $task . "\n";
     }
 }
+function addTask(&$inputTasks) {
+  $newTask = readline("Ievadiet jaunu uzdevumu: \n");
+  array_push($inputTasks, $newTask);
+}
+function viewTask($inputTasks){
+  $nr = readline("Ievadiet uzdevuma indeksu: \n");
+  echo $inputTasks[$nr] . "\n";
+}
 
-$name = readline("Ievadiet vardu!\n");
-echo "Your name is $name";
-
-// PIRMAIS IZDEVUMS 
-// DO WHILE CIKLS
-
-do {
-    echo "Number: $x \n";
-    $x++;
-  } while ($x <= 5);
-
-
-// DO WHILE CIKLS KURU APSTADINA IEVADOT "NNNNNNNNNNN"
+// DO WHILE CIKLS KURU APSTADINA IEVADOT "N"
 
 do {
-    echo "Uzdevumu pārvaldnieks\n";
-    echo "Ievadīt jaunu uzdevumu => 1\n";
-    echo "Apskatit uzdevumu => 2\n";
-    echo "Apskatīt uzdevumu => 3\n"
-
-    $choice = readline("Izēlies darbību");
-    
-
-    switch ($choice) {
-        case '1':
-          echo showAllTasks
+  echo "Uzdevumu pārvaldnieks\n";
+  echo "Apskatīt visus uzdevumu - 1\n";
+  echo "Ievadīt jaunu uzdevumu - 2\n";
+  echo "Apskatīt vienu uzdevumu - 3\n";
+  echo "Iziet - N\n";
+  $choice = readline("Enter value: ");
+  switch ($choice) {
+      case '1':
+          showAllTasks($taskList);
           break;
-        case '2':
-          echo 'To be implemented!\n';
+      case '2':
+          addTask($taskList);    
           break;
-        case '3':
-          echo 'To be implemented!\n';
+      case '3': 
+          viewTask($taskList);
           break;
-        default:
-          echo 'To be implemented!\n';
-      }
-      
-      
-  } while ($continue != "N");
+      default:
+          echo "Invalid option!\n";
+          break;
+  }
 
-?>
+} while ($choice != "N" && $choice != "n");
